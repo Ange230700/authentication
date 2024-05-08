8776916ea04cf5bb8f66184c70f2bfdfd6e3ef7c
Ange230700 <kouakouangeericstephane@gmail.com>
2024-05-08 22:12:38 +0200

    working...

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./sass/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./Register";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
