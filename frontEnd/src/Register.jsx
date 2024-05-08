9298e80adae2745fb0d9dc308fa2d6930f53c6c5
Ange230700 <kouakouangeericstephane@gmail.com>
2024-05-08 21:53:29 +0200

    working...

--- frontEnd/src/Register.jsx
+++ frontEnd/src/Register.jsx
@@ -1,5 +1,49 @@
 function Register() {
-  return <div></div>;
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign-Up</h2>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type=""
+              placeholder=""
+              name=""
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type=""
+              placeholder=""
+              name=""
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type=""
+              placeholder=""
+              name=""
+              className="form-control rounded-0"
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
